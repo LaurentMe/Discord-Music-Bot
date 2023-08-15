e789382d2664da42f1a0aac4c70255b636cab2b8
LaurentMe <laurent.meersman@gmail.com>
2023-08-15 18:24:07 +0200

    Update skip command

--- commands/skip.js
+++ commands/skip.js
@@ -2,17 +2,12 @@ module.exports = {
     name: 'skip',
     description: 'Skip song',
     execute(message, args, client, distube) {
-        if (distube.queues.collection.size > 0) {
-            try {
-                distube.skip(message);
-            } catch (e) {
-                try {
-                    distube.stop();
-                    return message.channel.send('No songs left in queue.');
-                } catch (e) {
-                    message.channel.send(`${client.emotes.error} | Error: \`${e}\``);
-                }
-            }
+        const queue = distube.getQueue(message);
+        if (distube.queues.collection.size > 1) {
+            distube.skip(message);
+        } else {
+            queue.stop();
+            return message.channel.send('No songs left in queue.');
         }
     },
 };
